c414fb8655319fcf3fccd2865a0326271ed59126
oliviasv <62649380+oliviasv@users.noreply.github.com>
2020-10-29 23:50:01 +0200

    Initial commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?
+
+define('ID', '810355839797629');
+define('SECRET', '2dda257c5fbbb93f53d1c95207c3983e');
+define('URL', 'http://collaid.loc');
+
+?>
---
+++ index.php
@@ -0,0 +1,3 @@
+<? include 'config.php' ?>
+
+<a href="https://www.facebook.com/v2.9/dialog/oauth?client_id=<?=ID?>&redirect_uri=<?=URL?>&response_type=code&scope=public_profile,email,user_location" target="_blank">Auth fb</a>
